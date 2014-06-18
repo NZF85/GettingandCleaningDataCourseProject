622df6f0c49d1f54acd7806ad834716fd1b6f09a
David Shinn <david.shinn@gmail.com>
2014-06-17 23:39:37 -0400

    Consolidate code in run_analysis.R file
    Retrieval action is no longer separate.
    The run_analysis.R file will check for the
    source data existing or the zip file existing
    and take the necessary actions so that the source
    data resides in path "UCI HAR Dataset/"

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,57 @@
+SOURCE_URL = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+SOURCE_DEST = 'getdata_projectfiles_UCI_HAR_Dataset.zip'
+SOURCE_MANIFEST <- c('UCI HAR Dataset/',
+                     'UCI HAR Dataset/activity_labels.txt',
+                     'UCI HAR Dataset/features.txt',
+                     'UCI HAR Dataset/features_info.txt',
+                     'UCI HAR Dataset/README.txt',
+                     'UCI HAR Dataset/test',
+                     'UCI HAR Dataset/test/Inertial Signals',
+                     'UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt',
+                     'UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt',
+                     'UCI HAR Dataset/test/subject_test.txt',
+                     'UCI HAR Dataset/test/X_test.txt',
+                     'UCI HAR Dataset/test/y_test.txt',
+                     'UCI HAR Dataset/train',
+                     'UCI HAR Dataset/train/Inertial Signals',
+                     'UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt',
+                     'UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt',
+                     'UCI HAR Dataset/train/subject_train.txt',
+                     'UCI HAR Dataset/train/X_train.txt',
+                     'UCI HAR Dataset/train/y_train.txt')
+
+### This function will download/unzip the files if necessary.
+### It checks if all of the files exists.  If any is missing,
+### it checks if the zip file exists.  If the zip file is missing
+### it downloads the zip file and extracts the contents
+prepare_source_files <- function() {
+    if (!all(file.exists(SOURCE_MANIFEST))) {
+        message("All files not in expected location")
+        if (!file.exists(SOURCE_DEST)) {
+            message("Source Zipfile missing, downloading: ", SOURCE_DEST)
+            download.file(SOURCE_URL, SOURCE_DEST, method='curl')
+        }
+        message("Extracting ", SOURCE_DEST)
+        unzip(SOURCE_DEST, overwrite=TRUE)
+    }
+    message("All files ready in: UCI HAR Dataset/")
+    invisible()
+}
+
 combine_train_test <- function(filename_prefix, colClasses) {
     train_filename <- paste('UCI HAR Dataset/',
                             'train/',
